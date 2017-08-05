5b81bb028a457fdf24e8ddbf21e75d614f53404a
phaed <phaedrus.one@gmail.com>
2017-08-05 12:45:31 -0400

    Fixed bug with header

--- constant.py
+++ constant.py
@@ -8,11 +8,11 @@ def headers(referer):
     return {
         'Host': 'cccbdb.nist.gov',
         'Connection': 'keep-alive',
-        'Content-Length': 26,
+        'Content-Length': '26',
         'Pragma': 'no-cache',
         'Cache-Control': 'no-cache',
         'Origin': 'http://cccbdb.nist.gov',
-        'Upgrade-Insecure-Requests': 1,
+        'Upgrade-Insecure-Requests': '1',
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
         'Content-Type': 'application/x-www-form-urlencoded',
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
